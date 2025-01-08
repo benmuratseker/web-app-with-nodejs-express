893a7550763e2a9684de77692c671b4837d7e6a4
benmuratseker <benmuratseker@hotmail.com>
2025-01-08 20:57:33 +0100

    working with static files

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
 // app.use(morgan('combined'));
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));//this definition checks if we have an index page if we don't we see the message belove
 
 app.get('/', (req, res) => {
     res.send('Hello from my node.js app');
