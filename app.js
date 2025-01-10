20478d50c75506f48f3a6ea5ed9a2e38db91c451
benmuratseker <benmuratseker@hotmail.com>
2025-01-10 20:15:13 +0100

    ejs and index.ejs added

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));//this definition checks if we have an index page if we don't we see the message belove
 
+//set ejs as a template engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello from my node.js app!');
+    //res.send('Hello from my node.js app!');
+    res.render('index', { title: " XdLab", data: ['bachata', 'salsa', 'kizomba'] });// to access this page we need to delete such static files index.html 
 });
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
     // console.log('listening on port 3000');
     //console.log('listening on port: ' + chalk.green('3000'));
     // console.log(`listening on port: ${chalk.green('3000')}`);
