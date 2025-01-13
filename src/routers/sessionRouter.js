110319f9aa8c1972ff92723e3d5fc8a44252ce07
benmuratseker <benmuratseker@hotmail.com>
2025-01-13 22:34:14 +0100

    single session retrieved from mongo cloud

--- src/routers/sessionRouter.js
+++ src/routers/sessionRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const debug = require('debug')('app:sessionRouter');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const sessions = require('../data/sessions.json'); 
 
 const sessionsRouter = express.Router();
@@ -16,7 +16,8 @@ sessionsRouter.route('/')
         //     { title: 'Session 3', description: 'this is session 3'},
         //     { title: 'Session 4', description: 'this is session 4'},
         // ]});
-        const url = `mongodb+srv://dbUser:${password}@testcluster.efaao.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster`;
+    
+    const url = `mongodb+srv://dbUser:${password}@testcluster.efaao.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster`;
 
     const dbName = 'globomantics';//will be created under TestCluster on mongo
 
@@ -49,7 +50,28 @@ sessionsRouter.route('/:id')
     .get((req, res) => {
         const id = req.params.id;
         //res.send('hello single sessions ' + id);
-        res.render('session', { session: sessions[id] })
+
+        const url = `mongodb+srv://dbUser:${password}@testcluster.efaao.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster`;
+
+        const dbName = 'globomantics';//will be created under TestCluster on mongo
+
+        (async function mongo() {
+            let client;
+            try {
+                client = await MongoClient.connect(url);
+                debug('Connected to the Mongo Db');
+
+                const db = client.db(dbName);
+
+                const session = await db.collection('sessions').findOne({_id: new ObjectId(id)});//we have objectid in mongo
+                res.render('session', { session })
+            } catch (error) {
+                debug(error.stack);
+            }
+            client.close();
+        }())
+
+        // res.render('session', { session: sessions[id] }) // from json file
     });
 
 module.exports = sessionsRouter;
