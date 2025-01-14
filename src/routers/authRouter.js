82b714654bea13b73d120e16d89a5717e1b6566a
benmuratseker <benmuratseker@hotmail.com>
2025-01-14 22:51:17 +0100

    user create added for users collection

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -3,14 +3,42 @@ const debug = require('debug')('app:sessionRouter');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const authRouter = express.Router();
+const mongoPass = encodeURIComponent("S8qaze@Y38FFZ*u");
 
 authRouter.route('/signUp')
     .post((req, res)=>{
         //res.json(req.body);
         //TODO create user
-        req.login(req.body, () => {
-            res.redirect('/auth/profile');//create user and redirect to profile
-        });
+        const {username, password} = req.body;
+        const url = `mongodb+srv://dbUser:${mongoPass}@testcluster.efaao.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster`;
+        const dbName = 'globomantics';
+
+        (async function addUser(){
+            let client;
+            try {
+                client = await MongoClient.connect(url);
+
+                const db = client.db(dbName);
+                const user = {username, password};
+                const results = await db.collection('users').insertOne(user);//await for result not to break user create
+                debug(results);
+
+                // req.login(results.ops[0], () => {
+                    req.login(results.body[username], () => {
+                    debug(res);
+                    res.redirect('/auth/profile');//create user and redirect to profile
+                });
+
+            } catch (error) {
+                debug(error);
+            }
+            client.close();
+        }())
+
+
+        // req.login(req.body, () => {
+        //     res.redirect('/auth/profile');//create user and redirect to profile
+        // });
     });
 
 authRouter.route('/profile')
